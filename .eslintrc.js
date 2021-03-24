2e2abce665832ca21d903ad2dc147b9bc83cfce8
Christian Werner <ccwerner96@gmail.com>
2021-03-24 09:47:21 -0300

    adds custom eslint rules for semi & quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
+    es2020: true,
     node: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
@@ -10,5 +12,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: 2,
+    quotes: 2,
   },
 };
