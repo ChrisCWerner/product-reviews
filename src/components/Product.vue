f505daad8fffc76c0280a62be37e7eae2b840d57
Christian Werner <ccwerner96@gmail.com>
2021-03-26 15:01:57 -0300

    adds star rating and improvements to product layout

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,48 +2,77 @@
   <v-card tile>
     <v-card-title v-text="name" />
     <v-row no-gutters>
-      <v-col class="pa-4" cols="12" sm="3" xl="2">
-        <v-row no-gutters>
+      <v-col class="px-4" cols="12" sm="3" xl="2">
+        <!-- <v-row no-gutters>
           <v-col cols="9" sm="12">
             <v-img :src="image"></v-img>
           </v-col>
           <v-col cols="3" sm="12">
             <div class="">
-              <span>{{ review_count }} reviews</span>
+              <StarRating :stars="Number(star_rating)" />
             </div>
-            <div class="">
-              <span>{{ star_rating }}/5 rating</span>
-            </div>
-            <div class="">
-              <span>{{ available_count }} available</span>
+            <div class="px-1 my-n2">
+              <i class="text--secondary text-caption"
+                >from {{ review_count }} reviews</i
+              >
             </div>
           </v-col>
-        </v-row>
+        </v-row> -->
+        <div class="d-flex flex-row flex-sm-column">
+          <v-img class="flex-shrink-1" :src="image"></v-img>
+          <div class="align-self-end text-end" style="min-width: 120px">
+            <div class="">
+              <StarRating :stars="Number(star_rating)" />
+            </div>
+            <div class="px-1 my-n2">
+              <i class="text--secondary text-caption"
+                >from {{ review_count }} reviews</i
+              >
+            </div>
+          </div>
+        </div>
       </v-col>
       <v-col cols="0" sm="9">
-        <v-sheet>
-          <v-card-subtitle class="pb-0 pt-4">Description</v-card-subtitle>
-          <v-card-text v-text="description" />
-        </v-sheet>
+        <!-- <v-card-subtitle class="pb-0 pt-4">Description</v-card-subtitle> -->
+        <v-card-text v-text="description" />
+        <span class="text-h4 ml-4 mr-2">{{ price }}</span>
+        <i class="text--secondary text--lighten-3"
+          >{{ available_count }} available</i
+        >
       </v-col>
     </v-row>
+    <div class="footnotes px-1 d-flex flex-wrap align-center justify-end">
+      <div class="mr-3">
+        <v-icon size="14" class="mr-1">mdi-map-marker</v-icon>
+        <span class="text-caption font-italic" v-text="location" />
+      </div>
+      <div class="">
+        <v-icon size="14" class="mr-1">mdi-clock</v-icon>
+        <span class="text-caption font-italic" v-text="last_updated" />
+      </div>
+    </div>
   </v-card>
 </template>
 
 <script>
+import StarRating from "./StarRating";
+
 export default {
   props: {
     id: String,
-    name: String,
-    description: String,
-    last_updated: String,
-    available_count: Number,
-    price: String,
+    name: String, // OK
+    description: String, // OK
+    last_updated: String, // OK
+    available_count: Number, // ~~
+    price: String, // ~~
     location: String,
-    image: String,
+    image: String, // OK
     seller_name: String,
-    star_rating: [String, Number],
-    review_count: Number,
+    star_rating: [String, Number], // OK
+    review_count: Number, // ~~
+  },
+  components: {
+    StarRating,
   },
 };
 </script>
---
+++ src/components/StarRating.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="d-inline stars">
+    <v-icon
+      class="star-icon"
+      color="accent"
+      v-for="(star, i) in starArray"
+      :key="i"
+      >{{ star }}</v-icon
+    >
+  </div>
+</template>
+<script>
+export default {
+  name: "StarRating",
+  props: {
+    totalStars: {
+      type: Number,
+      default: 5,
+    },
+    stars: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    starArray() {
+      const stars = [];
+      let starControl = this.stars;
+      for (let i = 0; i < this.totalStars; i++) {
+        if (starControl >= 1) {
+          stars.push("mdi-star");
+        } else if (starControl > 0) {
+          stars.push("mdi-star-half-full");
+        } else {
+          stars.push("mdi-star-outline");
+        }
+        starControl--;
+      }
+      return stars;
+    },
+  },
+};
+</script>
+<style scoped>
+.stars {
+  padding: 0 3px;
+}
+.star-icon {
+  margin: 0 -3px;
+}
+</style>
